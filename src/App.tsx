4cb60577bdb2cc9fbf68b9f3e955912863ec4188
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-12 17:43:01 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2 className='text-red-500'>HIII</h2>
+    </div>
+  )
+}
+
+export default App
